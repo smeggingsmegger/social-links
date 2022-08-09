b438d1356b4775590567dd7e05006e7fe1573072
Scott Blevins <sblevins@gmail.com>
2022-08-09 15:35:02 -0500

    Adding pinterest, fixing FB

--- src/profiles/facebook.ts
+++ src/profiles/facebook.ts
@@ -12,6 +12,14 @@ export const facebook = {
       match: '(https?://)?m.facebook.com/({PROFILE_ID})/?', group: 2, type: TYPE_MOBILE,
       pattern: 'https://m.facebook.com/{PROFILE_ID}'
     },
+    {
+      match: '(https?://)?(www.)?facebook.com/profile\.php\?id\=({PROFILE_ID})/?', group: 3, type: TYPE_DESKTOP,
+      pattern: 'https://facebook.com/{PROFILE_ID}'
+    },
+    {
+      match: '(https?://)?m.facebook.com/profile\.php\?id\=({PROFILE_ID})/?', group: 2, type: TYPE_MOBILE,
+      pattern: 'https://m.facebook.com/{PROFILE_ID}'
+    },
     { match: '({PROFILE_ID})', group: 1 },
   ]
 };
